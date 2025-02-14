2e56ddbfa48f395c834e327e2d9f129547cb2bed
Guilherme Onizio <yguih011@gmail.com>
2025-02-13 23:04:24 -0300

    added validation if an account already exists

--- view.py
+++ view.py
@@ -1,10 +1,18 @@
-from models import Conta, engine
+from models import Conta, engine, Bancos
 from sqlmodel import Session, select
 
 def criar_conta(conta: Conta):
     with Session(engine) as session:
+        statement = select(Conta).where(Conta.banco == conta.banco)
+        results = session.exec(statement).all()
+        
+        if results:
+            print('Já existe uma conta com esse banco')
+            return
+
         session.add(conta)
         session.commit()
+        return conta
 
-conta = Conta(valor=10, banco='Nubank')
+conta = Conta(valor=10, banco=Bancos.INTER)
 criar_conta(conta)
